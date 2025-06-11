9e5813b5e10b7025ac3a636124f25f57ef6150d7
virtualp1 <zininnikita309@gmail.com>
2025-06-11 10:07:34 +0400

    init pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Blackbox',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,ts,css,html,ico,png,svg,webmanifest}'],
+      },
+    }),
+  ],
 })
