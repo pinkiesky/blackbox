13599bae6d0f910799b59712802f95b7b4686abf
virtualp1 <zininnikita309@gmail.com>
2025-06-22 15:16:29 +0400

    feat: add LogChartSettings component; create initial structure for log chart settings

---
+++ src/components/LogChartSettings/LogChartSettings.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react'
+
+const LogChartSettings: FC = () => {
+  return (
+    <div>
+      <h2>Log Chart Settings</h2>
+      <p>Settings for the log chart will go here.</p>
+      {/* Add your settings components here */}
+    </div>
+  )
+}
+
+export default LogChartSettings
