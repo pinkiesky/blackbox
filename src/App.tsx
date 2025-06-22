ecf83759e91d6e0ee1d7c64db2b1ce20dae7fd42
Aleksandr Zinin <fontercity@gmail.com>
2025-06-22 17:33:16 +0400

    feat: implement calculateStatistic function for enhanced log data analysis

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,10 @@ import type { DraggableSelectEvent } from '@/utils/chart'
 import { resampleData } from './parse/resampler/resampler'
 import { parseEdgeTxLogs } from './parse/edgetx/parseEdgeTxLog'
 import type { Log, LogStatistics } from './parse/types'
-import { ValueStatCalculator } from './math/ValueStatCalculator'
-import { DistanceCalculator } from './math/DistanceCalculator'
-import { DerivativeCalculator } from './math/DerivativeCalculator'
 import type { GetSegmentConfigOptions } from './components/MapPolylines/MapLogPathRenderer'
 import { interpolateHsl } from 'd3-interpolate'
 import type { Segment } from './types/data'
+import { calculateStatistic } from './math/calculateStatistic'
 
 const styles: Record<string, SxProps> = {
   app: {
@@ -100,64 +98,28 @@ function App() {
       return null
     }
 
-    const altitudeCalculator = new ValueStatCalculator()
-    const speedCalculator = new ValueStatCalculator()
-    const transmitterPowerCalculator = new ValueStatCalculator()
-    const transmitterQualityCalculator = new ValueStatCalculator()
-    const distanceCalculator = new DistanceCalculator()
-    const rollDerivativeCalculator = new DerivativeCalculator()
+    const stats = calculateStatistic(rawLog)!
 
-    for (let i = 0; i < rawLog.records.length; i++) {
-      const record = rawLog.records[i]
-      const prevRecord = rawLog.records[i - 1] || null
-      const weight = prevRecord
-        ? record.flightTimeSec - prevRecord.flightTimeSec
-        : 1
-
-      altitudeCalculator.addValueWeighted(record.altitudeM, weight)
-      speedCalculator.addValueWeighted(record.groundSpeedKmh, weight)
-      transmitterPowerCalculator.addValueWeighted(
-        record.transmitterPowerMw,
-        weight,
-      )
-      transmitterQualityCalculator.addValueWeighted(
-        record.transmitterLinkQuality,
-        weight,
-      )
-      distanceCalculator.addPoint(record.coordinates, record.altitudeM)
-      rollDerivativeCalculator.addValue(record.rollRad, weight)
-    }
-
-    console.log('Altitude stats:', altitudeCalculator.getValue())
-    console.log('Speed stats:', speedCalculator.getValue())
-    console.log(
-      'Transmitter Power stats:',
-      transmitterPowerCalculator.getValue(),
-    )
-    console.log(
-      'Transmitter Quality stats:',
-      transmitterQualityCalculator.getValue(),
-    )
-    console.log(
-      'Total distance:',
-      distanceCalculator.getDistance().totalDistanceM,
-      'm',
-    )
-    console.log(
-      'Roll Derivative stats:',
-      rollDerivativeCalculator.getDerivativeData(),
-    )
-
-    const stats: LogStatistics = {
-      altitude: altitudeCalculator.getValue(),
-      groundSpeedKmh: speedCalculator.getValue(),
-      transmitterPowerMw: transmitterPowerCalculator.getValue(),
-      transmitterLinkQuality: transmitterQualityCalculator.getValue(),
-      totalDistanceM: distanceCalculator.getDistance().totalDistanceM,
-    }
+    console.log('Altitude stats:', stats?.altitude)
+    console.log('Speed stats:', stats?.groundSpeedKmh)
 
     return stats
   }, [rawLog])
+
+  useMemo<LogStatistics | null>(() => {
+    if (!rawLog || !selectedRange) {
+      return null
+    }
+
+    const [start, end] = selectedRange
+    const stats = calculateStatistic(rawLog, {
+      fromSec: start,
+      untilSec: end,
+    })!
+    console.log('Selected range stats:', stats)
+
+    return stats
+  }, [rawLog, selectedRange])
 
   const parseRawData = async (rawData: string): Promise<Log> => {
     console.log('Parsing raw data...', rawData.length, 'characters')
